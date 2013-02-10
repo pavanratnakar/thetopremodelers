1d45c43d8a0f9c4405021999e4a01bc12e94ef9d
Pavan Ratnakar <pavanratnakar@gmail.com>
2013-02-10 23:54:41 +0530

    zopim integration

--- class/page.class.php
+++ class/page.class.php
@@ -178,7 +178,7 @@ class Page{
             $return.=$this->printJS('herve_'.$this->currentPage->class.'_js');
         }
         $return.=$this->printGA();
-        //$return.=$this->zopimChat();
+        $return.=$this->zopimChat();
         $return.='</body></html>';
         return $return;
     }
@@ -231,6 +231,17 @@ class Page{
     }
     public function facebookComment($data) {
         return '<div class="fb-comments" data-href="'.$data['href'].'" data-width="'.$data['width'].'" data-num-posts="'.$data['posts'].'"></div>';
+    }
+    public function zopimChat(){
+        return "<!--Start of Zopim Live Chat Script-->
+        <script type=\"text/javascript\">
+        window.\$zopim||(function(d,s){var z=\$zopim=function(c){z._.push(c)},$=z.s=
+        d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+        _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+        $.src='//cdn.zopim.com/?MddEPZHtRdlfhytSBoJLIVoWotreXFR0';z.t=+new Date;$.
+        type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+        </script>
+        <!--End of Zopim Live Chat Script-->";
     }
 }
 ?>
